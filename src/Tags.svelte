7d0d96a4b7d92b6e137b9457130becd65b2f2729
David Hunt <git@dnotes.net>
2022-08-16 11:03:19 -0700

    fixes agustinl/svelte-tags-input#68
    - adds default outline around tags input layout

--- src/Tags.svelte
+++ src/Tags.svelte
@@ -429,6 +429,11 @@ function uniqueID() {
     border: solid 1px #000;    
 }
 
+.svelte-tags-input-layout:focus-within {
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+}
+
 /* svelte-tags-input */
 
 .svelte-tags-input {
